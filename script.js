d8efcbdce9a356993090a8142baf460f28097e5a
Budi Saputra <kursdemy.budisaputra@gmail.com>
2019-01-06 18:52:58 +0700

    Javascript Variable

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World !!")
+// console.log("Hello World !!")
+
+document.getElementById('heading').innerHTML = "Hello, World!"
+
+//variable
+
+var name = "Budi";
+console.log(name);
+
+var lastname = "Saputra";
+console.log (lastname);
+
+var age = 26;
+console.log(age);
+
+var fullname = name + ' ' + lastname ;
+console.log (fullname);
+
+var fullname = true ;
+console.log (fullname);
+
+var job = "Trainer";
+console.log (job);
+
+var bio = "Nama Saya" + " " + name + " " + lastname + " Umur saya " + age + " " + "Pekerjaan saya adalah" + " " + job ;
+console.log (bio);
