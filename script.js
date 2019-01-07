e036ed03d6b06030a988f777995dd8d28638e2d9
Budi Saputra <kursdemy.budisaputra@gmail.com>
2019-01-07 19:56:00 +0700

    Javascript Operator

--- script.js
+++ script.js
@@ -2,25 +2,106 @@
 
 document.getElementById('heading').innerHTML = "Hello, World!"
 
-//variable
+//* variable
 
-var name = "Budi";
-console.log(name);
+// var name = "Budi";
+// console.log(name);
 
-var lastname = "Saputra";
-console.log (lastname);
+// var lastname = "Saputra";
+// console.log (lastname);
 
-var age = 26;
-console.log(age);
+// var age = 26;
+// console.log(age);
 
-var fullname = name + ' ' + lastname ;
-console.log (fullname);
+// var fullname = name + ' ' + lastname ;
+// console.log (fullname);
 
-var fullname = true ;
-console.log (fullname);
+// var fullname = true ;
+// console.log (fullname);
 
-var job = "Trainer";
-console.log (job);
+// var job = "Trainer";
+// console.log (job);
 
-var bio = "Nama Saya" + " " + name + " " + lastname + " Umur saya " + age + " " + "Pekerjaan saya adalah" + " " + job ;
-console.log (bio);
+// var bio = "Nama Saya" + " " + name + " " + lastname + " Umur saya " + age + " " + "Pekerjaan saya adalah" + " " + job ;
+// console.log (bio);
+
+//* Operator
+
+// var x = 5;
+// var y = 7;
+// var z = x + y ;
+// console.log(z);
+
+// var x = 5 ;
+// var y = 7 ;
+// var z = x * y ;
+// console.log(z);
+
+// macam-macam Operator aritmetik
+
+// +
+// -
+// /
+// %
+// ++ --> increment
+// -- --> decrement
+
+// assigment operator
+
+// Operator    Contoh      Sama seperti
+// =           x = y       x = y
+// +=          x += y      x = x + y
+// -=          x -= y      x = x - y
+// *=          x *= y      x = x * y
+// /=          x /= y      x = x / y
+// %=          x %= y      x = x % y
+
+// var x = 10;
+// x += 5;
+// console.log(x);
+
+
+// String Operator
+
+// text1 = "Budi";
+// text2 = "Saputra";
+// text3 = text1 + " " + text2;
+// console.log(text3);
+
+// x = 5 + 5 ;
+// y = "5" + 5 ;
+// z = "Hello" + 5 ;
+// console.log(x);
+// console.log(y);
+// console.log(z);
+
+// comparison Operator
+
+// ==      equal to (value)
+// ===     equal value and equal type
+// !=      not equal
+// !==     not equal value atau not equal type
+// >       greather than
+// <       less than
+// >=      greather than atau equal to
+// <=      less than atau equal
+// ?       ternary operator
+
+
+// logical operator
+
+// &&      logical and
+// ||      logical or
+// !       logical not
+
+// Type operator
+
+// typeof              return --> tipe
+// instanceof          return --> bolean
+
+// Bitwise
+
+// & | ~ ^ << >> >>>
+
+
+
