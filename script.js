d93c561acc11d03e3ec45a6d797e6cc7e6bb9067
Budi Saputra <kursdemy.budisaputra@gmail.com>
2019-01-10 19:42:35 +0700

    Menyisipkan Object dan Array

--- script.js
+++ script.js
@@ -394,3 +394,31 @@ Javascript Object
 //     "Orange",
 //     "Pear"
 // ];
+
+/*---------------------------------------------------------------
+Menyisipkan Array dan Object
+---------------------------------------------------------------*/
+
+var car = {
+    made: "Honda",
+    speed: 160,
+    enggine: {
+        size: 2.5,
+        made: "Daihatsu",
+        fuel: "Pertamax",
+        piston: [
+            "2018",
+            {make: "China"},
+            {make: ""}
+        ],
+    },
+    drive: function(){ return "drive";}
+};
+
+var array = [
+    "String",
+    1000,
+    [true, 100],
+    {name: "Budi"},
+    function() { return "drive";}
+];
